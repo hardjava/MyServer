fc40ec5356c7ee9efa82c92f70c3f4a99d4d4c0c
soohyeon <qkr1116510@naver.com>
2023-11-17 21:33:43 +0900

    11.17 21:31 soo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import java.text.ParseException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         AccommodationReservationApp app = new AccommodationReservationApp();
         app.run();
     }
