00db269fb09c2fd54ef8cdc43c3d8c8121b56d91
Song <fkdlcn123@naver.com>
2023-11-24 00:15:24 +0900

    11

---
+++ src/main/java/airbnb/persistence/dto/MoreHouseInfoDTO.java
@@ -0,0 +1,17 @@
+package airbnb.persistence.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Setter
+@Getter
+
+public class MoreHouseInfoDTO implements Serializable {
+    private List<AmenitiesDTO> amenitiesDTOList;
+    private FeePolicyDTO feePolicyDTO;
+    private List<ReservationDTO> reservationDTOList;
+    private List<ReviewDTO> reviewDTOList;
+}
