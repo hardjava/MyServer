404a3c6ae139e3a27dbabd9244be86be195ac5cb
soohyeon <qkr1116510@naver.com>
2023-11-25 01:56:01 +0900

    Song 님이 변경한 내용

--- src/main/java/airbnb/persistence/dto/HouseAndDiscountDTO.java
+++ src/main/java/airbnb/persistence/dto/HouseAndDiscountDTO.java
@@ -1,15 +1,34 @@
 package airbnb.persistence.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.List;
+import java.io.Serializable;
 
 @Setter
 @Getter
-@ToString
 @Builder
 @AllArgsConstructor
-public class HouseAndDiscountDTO {
+public class HouseAndDiscountDTO implements Serializable {
     private HouseDTO houseDTO;
     private DiscountPolicyDTO discountPolicyDTO;
+
+    public HouseAndDiscountDTO(int houseId, int hostId, String houseName, String houseAddress, String houseIntroduce, int bedroom, int bathroom,
+                               Integer discountDay, Integer discount_amount, Integer discount_rate) {
+        this.houseDTO = new HouseDTO(hostId, houseName, houseAddress, houseIntroduce, bedroom, bathroom);
+        if (discountDay == null)
+            discountDay = discount_amount = discount_rate = 0;
+        else {
+            if (discount_amount == null)
+                discount_rate = 0;
+            if (discount_rate == null)
+                discount_rate = 0;
+        }
+        this.discountPolicyDTO = new DiscountPolicyDTO(discountDay, discount_amount, discount_rate, houseId);
+    }
+    public String toString() {
+        return String.format("%-20s%-10d%-15d%-15d", houseDTO.getHouseName(), discountPolicyDTO.getDiscountDay(), discountPolicyDTO.getDiscount_amount(), discountPolicyDTO.getDiscount_rate());
+    }
 }
